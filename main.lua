b839e38cb73a11391e52ee5f89c40f56ccc8badc
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 15:34:38 -0500

    refactored conditions

--- main.lua
+++ main.lua
@@ -3,11 +3,10 @@ condition = -25
 
 if condition > 0 then
     message = 1
-end
-
-if condition <= 0 then
+else
     message = -1
 end
+
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
