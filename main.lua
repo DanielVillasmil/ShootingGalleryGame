582d94c5b6f21dba24ecf77dedc483d875f4893d
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 15:32:09 -0500

    created another condition

--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@ if condition > 0 then
     message = 1
 end
 
+if condition <= 0 then
+    message = -1
+end
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
