3852ec30dbe1457c5d820f8f1e243994b917d2a5
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 16:10:47 -0500

    added value to table

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ message = 0
 
 testScores = {}
 
+testScores[1] = 95
+testScores[2] = 87
+testScores[3] = 98
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
