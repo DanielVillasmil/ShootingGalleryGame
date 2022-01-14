ed116cea6ed3cb0c17f2ccffa27c3750315b6098
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 16:03:04 -0500

    begining of simple global/local variables

--- main.lua
+++ main.lua
@@ -1,19 +1,6 @@
 message = 0
-chicken = 0
-
-function increaseMessage(i)
-    message = message + i
-end
-
-function double(val)
-    val = val * 2
-    return val
-end
-
-message = double(12)
-chicken = double(message)
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(chicken)
+    love.graphics.print(message)
 end
