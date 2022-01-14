663b64162aba668a8f34412e18052102b91431c0
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 16:11:34 -0500

    printed value of index 2

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ testScores[1] = 95
 testScores[2] = 87
 testScores[3] = 98
 
+message = testScores[2]
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
