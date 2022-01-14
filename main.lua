4ec0d30164b5c15e076d5a2486640c8e40475b8c
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 15:24:36 -0500

    proved changing a simple variable

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 message = 5
 chicken = 10
-output = chicken + 1
+output = chicken * 3
+
+message = message - 1
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(output)
+    love.graphics.print(message)
 end
