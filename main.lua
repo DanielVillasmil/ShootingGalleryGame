977563648ab02660015924dcbcd27e0d4600d271
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 15:43:08 -0500

    added another variable to while loop

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 message = 0
+test = 0
 
-while message < 10 do
+while message < 100 do
     message = message + 1
+    test = test - 5
 end
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(message)
+    love.graphics.print(test)
 end
