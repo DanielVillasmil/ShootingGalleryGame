81dc47abf4b8889938f3a83e5788492bc903fd31
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 19:35:33 -0500

    created filled rectangle

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -1,11 +1,11 @@
 function love.load()
-    number = 0
+    
 end
 
 function love.update(dt)
-    number = number + 1
+    
 end
 
 function love.draw()
-
+    love.graphics.rectangle("fill", 0, 0, 200, 100)
 end
