94ced2dadf8752e9c8b649f9027ae006abd25396
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 19:51:06 -0500

    finished shapes and color

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -7,8 +7,9 @@ function love.update(dt)
 end
 
 function love.draw()
+    -- If copy RGB divide each value by 255
     love.graphics.setColor(0.5, 0.8, 0)
-    love.graphics.rectangle("fill", 200, 400, 200, 100)
+    love.graphics.rectangle("fill", 200, 250, 200, 100)
     
     love.graphics.setColor(0.5, 0, 0.8)
     love.graphics.circle("fill", 300, 200, 100)
