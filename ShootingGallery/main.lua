c99f9a32a8507a5659cb483d5b21f22dff0e2bbb
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 19:57:55 -0500

    added print for score and color

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -14,6 +14,10 @@ end
 
 function love.draw()
     -- If copy RGB divide each value by 255
-    love.graphics.circle("fill", target.x, target.y, target.radius)   
     love.graphics.setColor(1, 0, 0) 
+    love.graphics.circle("fill", target.x, target.y, target.radius)  
+
+    love.graphics.setColor(1, 1 ,1)
+    love.graphics.print(score, 0, 0)
+
 end
