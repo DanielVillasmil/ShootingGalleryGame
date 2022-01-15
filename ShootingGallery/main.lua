c16598c5dccdb6f93b5b3e80a5afe13c408ee404
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 19:46:27 -0500

    added color to shapes

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -7,6 +7,7 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setColor(0.5, 0.8, 0)
     love.graphics.rectangle("fill", 200, 400, 200, 100)
     love.graphics.circle("fill", 300, 200, 100)
 end
