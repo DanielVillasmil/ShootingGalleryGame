be89bc506e04a5ebbbd0309ab90db37eda372ff7
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-14 19:47:13 -0500

    added a color for each shape

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -9,5 +9,7 @@ end
 function love.draw()
     love.graphics.setColor(0.5, 0.8, 0)
     love.graphics.rectangle("fill", 200, 400, 200, 100)
+    
+    love.graphics.setColor(0.5, 0, 0.8)
     love.graphics.circle("fill", 300, 200, 100)
 end
